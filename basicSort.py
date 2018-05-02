1d094be970cdeede4cd800807ad3d365da43f2e0
lifeinvader00 <abhinavramkumar91@gmail.com>
2018-05-02 13:00:45 +0530

    2 morea added

---
+++ basicSort.py
@@ -0,0 +1,19 @@
+# Write a program that accepts a comma separated sequence of words as input and prints the words in a comma-separated sequence after sorting them alphabetically.
+# Suppose the following input is supplied to the program:
+# without,hello,bag,world
+# Then, the output should be:
+# bag,hello,without,world
+
+# Hints:
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+
+
+def basicSort(string):
+    list = string.split(",")
+    list.sort()
+    return ", ".join(list)
+
+
+words = input("Enter a comma separated list of words  ")
+
+print(basicSort(words))
