3fabae80473848470d020ee1666b3bf0fa662921
lifeinvader00 <abhinavramkumar91@gmail.com>
2018-05-04 01:14:45 +0530

    Added 2 and half programs

---
+++ listOverlap.py
@@ -0,0 +1,14 @@
+# Take two lists, say for example these two:
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+# and write a program that returns a list that contains only the elements that
+# are common between the lists (without duplicates). Make sure your program
+# works on two lists of different sizes.
+
+# Extras:
+# Randomly generate two lists to test this
+
+l1 = [1, 3, 4, 5, 9]
+l2 = [2, 1, 3, 7, 9, 2, 3]
+
+print(zip(l1, l2))
